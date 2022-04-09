245c8ad8d4a836452e34685e3569229b63b0118a
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-04-09 08:10:59 +0100

    added licence badge to readme

--- src/Project/Program.cs
+++ src/Project/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Serilog;
+
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateBootstrapLogger();
