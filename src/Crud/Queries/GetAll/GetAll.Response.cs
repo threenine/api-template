1f679b125b2340e8e590a1fd1e536d6dc2fed0d6
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-06-26 22:55:38 +0100

    Fixed Bug In DTO

--- src/Crud/Queries/GetAll/GetAll.Response.cs
+++ src/Crud/Queries/GetAll/GetAll.Response.cs
@@ -2,7 +2,7 @@ namespace Namespace.Resource.Queries.GetAll;
 
 public class Response
 {
-    public List<Entity> Entity { get; set; }
+    public List<DTO> DTO { get; set; }
 }
 
 public class ChargeRate
